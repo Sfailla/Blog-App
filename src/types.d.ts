a491dbbebad5819d71f895f212ceeec8d4e2f590
Sfailla <Sfailla1983@gmail.com>
2021-11-26 20:16:05 -0500

    added custom types file

---
+++ src/types.d.ts
@@ -0,0 +1,14 @@
+///// STYLED COMPONENTS TYPES /////
+import { CSSProp } from 'styled-components'
+import theme from './styles/theme'
+
+declare module 'styled-components' {
+  type Theme = typeof theme
+  export interface DefaultTheme extends Theme {}
+}
+
+declare module 'react' {
+  interface Attributes {
+    css?: CSSProp | CSSObject
+  }
+}
