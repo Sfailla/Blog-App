bb9e4e550de932893cf970edd3908557edd273b4
Sfailla <Sfailla1983@gmail.com>
2021-11-26 20:16:24 -0500

    added App Context for app provider

---
+++ src/contexts/AppContext.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../styles/theme'
+
+interface AppContextInterface {
+  mode: string
+  setMode: (mode: string) => void
+}
+
+const AppContext = React.createContext<AppContextInterface | null>(null)
+
+export function useContext(): AppContextInterface {
+  const context = React.useContext(AppContext)
+
+  if (!context) {
+    throw new Error('useContext must be used within a AppContextProvider')
+  }
+
+  return context
+}
+
+export function AppProvider({ children }: { children: React.ReactNode }): React.ReactElement {
+  const [mode, setMode] = React.useState<string>('light')
+  const contextValues = React.useMemo<AppContextInterface>(
+    () => ({ mode, setMode }),
+    [mode, setMode]
+  )
+
+  const themeMode = mode === 'light' ? theme.light : theme.dark
+
+  return (
+    <AppContext.Provider value={contextValues}>
+      <ThemeProvider theme={themeMode}>{children}</ThemeProvider>
+    </AppContext.Provider>
+  )
+}
