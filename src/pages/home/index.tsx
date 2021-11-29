19de16260f68b0e6869a3e6f90d4ec45c4584740
Sfailla <Sfailla1983@gmail.com>
2021-11-28 22:49:01 -0500

    removed navbar from home and added it to app component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Container } from './style'
 
-import { Navbar, Banner, ArticleFeed } from '../../components'
+import { Banner, ArticleFeed } from '../../components'
 
 function Home() {
   return (
     <Container>
-      <Navbar />
       <Banner />
       <ArticleFeed />
     </Container>
