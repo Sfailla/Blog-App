14a461f3e7bb82ef5742fb205db5b9b4b72282b9
Sfailla <Sfailla1983@gmail.com>
2021-12-04 11:48:56 -0500

    WIP: progress towards signup and signin components

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,12 +1,37 @@
 import React, { ReactElement } from 'react'
-import { Container } from './style'
+import { LayoutWrapper } from '../../styles/shared'
+import { Container, ContentWrapper, Title, FormContainer, FormGroup, Label, Input } from './style'
 
 interface Props {}
 
-export default function Signup({}: Props): ReactElement {
+export default function Signup({}): ReactElement {
   return (
     <Container>
-      <span>register page</span>
+      <LayoutWrapper
+        css={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <ContentWrapper>
+          <Title>Sign Up</Title>
+          <FormContainer>
+            <FormGroup>
+              <Label>Username</Label>
+              <Input type="text" placeholder="enter username..." />
+            </FormGroup>
+            <FormGroup>
+              <Label>Email</Label>
+              <Input type="email" placeholder="enter email..." />
+            </FormGroup>
+            <FormGroup>
+              <Label>Password</Label>
+              <Input type="password" placeholder="enter password..." />
+            </FormGroup>
+          </FormContainer>
+        </ContentWrapper>
+      </LayoutWrapper>
     </Container>
   )
 }
