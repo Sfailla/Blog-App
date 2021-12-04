3a06679bd4b92cd0e3f35ad1259dcabf36422013
Sfailla <Sfailla1983@gmail.com>
2021-12-04 11:47:57 -0500

    added route for signup

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Container } from './style'
-import { Homepage } from '../../pages'
+import { Homepage, Signup } from '../../pages'
+import { Navbar } from '../../components'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App(): React.ReactElement {
   return (
     <Container>
-      <Homepage />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </Container>
   )
 }
