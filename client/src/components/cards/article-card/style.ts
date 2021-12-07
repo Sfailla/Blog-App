dbf2caa9b5380c3a1220f1d93463706ab3d83b98
Sfailla <Sfailla1983@gmail.com>
2021-12-07 10:50:13 -0500

    styling progress

--- client/src/components/cards/article-card/style.ts
+++ client/src/components/cards/article-card/style.ts
@@ -3,12 +3,21 @@ import styled from 'styled-components/macro'
 export const Container = styled.li`
   width: 100%;
   height: fit-content;
+  list-style: none;
+  padding: 2rem 0;
 `
 
 export const Header = styled.div``
-export const AuthorDetails = styled.div``
+export const AuthorDetails = styled.div`
+  display: flex;
+`
 export const AuthorName = styled.h4``
-export const Avatar = styled.img``
+export const Avatar = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: lightblue;
+`
 export const CreatedDate = styled.span``
 export const FavoriteWrapper = styled.div``
 export const FavoriteCount = styled.span``
