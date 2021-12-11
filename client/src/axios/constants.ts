83c4ca0e1f1d054a989a1c67844fcdf0886ee0fc
Sfailla <Sfailla1983@gmail.com>
2021-12-11 16:22:15 -0500

    added axios component for axios instance and base routes

---
+++ client/src/axios/constants.ts
@@ -0,0 +1,8 @@
+export const baseUrl = 'http://localhost:3001/api/v1'
+
+export enum endpoints {
+  auth = 'users',
+  tags = 'tags',
+  articles = 'articles',
+  profiles = 'profiles'
+}
--- client/src/axios/index.ts
+++ client/src/axios/index.ts
@@ -1,1 +1,13 @@
 import axios, { AxiosRequestConfig } from 'axios'
+import { baseUrl } from './constants'
+
+interface JwtHeader {
+  Authorization?: string
+}
+
+export function getJWTHeader(token: string): JwtHeader {
+  return { Authorization: `x-auth-token ${token}` }
+}
+
+const config: AxiosRequestConfig = { baseURL: baseUrl }
+export const axiosInstance = axios.create(config)
