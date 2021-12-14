35cab82e8d6b2e07254a4804b5c6cab80501d859
Sfailla <Sfailla1983@gmail.com>
2021-12-14 14:24:34 -0500

    added validation for signup page

--- client/src/pages/signup/index.tsx
+++ client/src/pages/signup/index.tsx
@@ -2,26 +2,61 @@ import React, { ReactElement } from 'react'
 import { LayoutWrapper } from '../../styles/shared'
 import { Container, ContentWrapper, Title, FormContainer, FormGroup, Label, Input } from './style'
 
-// interface Props {}
+import { useFormValidation } from '../../hooks'
+import { validateSignup } from './validation'
+
+const initialValues = {
+  username: '',
+  email: '',
+  password: ''
+}
 
 export default function Signup(): ReactElement {
+  const { values, handleChange, handleSubmit } = useFormValidation(
+    initialValues,
+    validateSignup,
+    submit
+  )
+
+  function submit(): void {
+    console.log('form submitted')
+  }
+
   return (
     <Container>
       <LayoutWrapper>
         <ContentWrapper>
           <Title>Sign Up</Title>
-          <FormContainer>
+          <FormContainer onSubmit={handleSubmit}>
             <FormGroup>
               <Label>Username</Label>
-              <Input type="text" placeholder="enter username..." />
+              <Input
+                type="text"
+                name="username"
+                onChange={handleChange}
+                value={values.username}
+                placeholder="enter username..."
+              />
             </FormGroup>
             <FormGroup>
               <Label>Email</Label>
-              <Input type="email" placeholder="enter email..." />
+              <Input
+                type="email"
+                name="email"
+                onChange={handleChange}
+                value={values.email}
+                placeholder="enter email..."
+              />
             </FormGroup>
             <FormGroup>
               <Label>Password</Label>
-              <Input type="password" placeholder="enter password..." />
+              <Input
+                type="password"
+                name="password"
+                onChange={handleChange}
+                value={values.password}
+                placeholder="enter password..."
+              />
             </FormGroup>
           </FormContainer>
         </ContentWrapper>
---
+++ client/src/pages/signup/validation.ts
@@ -0,0 +1,21 @@
+import { OptionalFieldValues, ValidationErrors } from '../../../types/forms'
+
+export function validateSignup(values: OptionalFieldValues): ValidationErrors {
+  const errors: ValidationErrors = {}
+
+  if (!values.username) {
+    errors.username = 'username is required'
+  }
+
+  if (!values.email) {
+    errors.email = 'email is Required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+
+  if (!values.password) {
+    errors.password = 'password is required'
+  }
+
+  return errors
+}
