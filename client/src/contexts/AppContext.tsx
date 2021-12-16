5314d8d6102e70fd957be3e95f126752d573078a
Sfailla <Sfailla1983@gmail.com>
2021-12-16 09:50:22 -0500

    improved types for AppContext

--- client/src/contexts/AppContext.tsx
+++ client/src/contexts/AppContext.tsx
@@ -1,12 +1,11 @@
 import React, { ReactElement, ReactNode, useContext, createContext, useMemo, useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { theme, ThemeType } from '../styles/theme'
-
-type PreferredMode = 'light' | 'dark'
+import { Mode } from '../../types/shared'
 
 interface Context {
-  mode: PreferredMode
-  setMode: React.Dispatch<React.SetStateAction<PreferredMode>>
+  mode: Mode
+  setMode: React.Dispatch<React.SetStateAction<Mode>>
 }
 
 type ThemeMode = ThemeType['light'] | ThemeType['dark']
@@ -24,15 +23,14 @@ export function useAppContext() {
 }
 
 const prefersDark: string = '(prefers-color-scheme: dark)'
-const getPreferredTheme: () => PreferredMode = () =>
-  window.matchMedia(prefersDark).matches ? 'dark' : 'light'
+const getPreferredTheme = () => (window.matchMedia(prefersDark).matches ? 'dark' : 'light')
 
 interface Props {
   children: ReactNode
 }
 
 export function AppProvider({ children }: Props): ReactElement {
-  const [mode, setMode] = useState<PreferredMode>(getPreferredTheme())
+  const [mode, setMode] = useState<Mode>(getPreferredTheme())
   const contextValues: Context = useMemo(() => ({ mode, setMode }), [mode, setMode])
   const themeMode: ThemeMode = theme[mode]
 
