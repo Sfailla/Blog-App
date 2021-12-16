a9467349545eb2bc63f971cae54ad29ccd528a3b
Sfailla <Sfailla1983@gmail.com>
2021-12-16 09:49:39 -0500

    added more test setup.. exported userEvent and added support for watchMedia window property

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -4,6 +4,8 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
 import '@testing-library/user-event'
+// import axios from 'axios'
+// axios.defaults.adapter = require('axios/lib/adapters/http')
 
 // mock window.matchMedia to prevent error due to not being available in jestDom
 beforeAll(() => {
--- client/src/test/test-utils.tsx
+++ client/src/test/test-utils.tsx
@@ -1,21 +1,15 @@
-import React, { FunctionComponent, ReactElement } from 'react'
+import { FunctionComponent, ReactElement } from 'react'
 import { render, RenderOptions, RenderResult } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { AppProvider } from '../contexts/AppContext'
 
-type CustomRenderOptions = {
-  renderOptions?: Omit<RenderOptions, 'wrapper'>
-}
+type CustomRenderOptions = Omit<RenderOptions, 'wrapper'>
 
-function customRender(
-  ui: ReactElement,
-  { ...renderOptions }: CustomRenderOptions = {}
-): RenderResult {
-  const Wrapper: FunctionComponent = ({ children }) => {
-    return <AppProvider>{children}</AppProvider>
-  }
-  return render(ui, { wrapper: Wrapper, ...renderOptions })
+function customRender(ui: ReactElement, options: CustomRenderOptions = {}): RenderResult {
+  const Wrapper: FunctionComponent = ({ children }) => <AppProvider>{children}</AppProvider>
+  return render(ui, { wrapper: Wrapper, ...options })
 }
 
 export * from '@testing-library/react'
 
-export { customRender as render }
+export { customRender as render, userEvent }
