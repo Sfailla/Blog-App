f03c0f683b945b5af939c0ad3e4412fb0a1903b7
Sfailla <Sfailla1983@gmail.com>
2022-01-30 11:10:07 -0500

    minor improvements to comments

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -1,11 +1,13 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+/*
+  jest-dom adds custom jest matchers for asserting on DOM nodes.
+  allows you to do things like:
+
+  expect(element).toHaveTextContent(/react/i)
+  
+  learn more: https://github.com/testing-library/jest-dom
+*/
 import '@testing-library/jest-dom'
 import '@testing-library/user-event'
-// import axios from 'axios'
-// axios.defaults.adapter = require('axios/lib/adapters/http')
 
 // mock window.matchMedia to prevent error due to not being available in jestDom
 beforeAll(() => {
