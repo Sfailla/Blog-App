ea533dde43b32315681858f4cb7eef15eaff1618
Sfailla <Sfailla1983@gmail.com>
2022-01-30 21:35:58 -0500

    finished setting up msw. added server config and handlers

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -9,6 +9,28 @@
 import '@testing-library/jest-dom'
 import '@testing-library/user-event'
 
+import { setupServer } from 'msw/node'
+import { handlers } from './test/server-handlers'
+
+const server = setupServer(...handlers)
+/*
+  server.listen()
+  Establishes a request interception
+  instance before all tests.
+*/
+beforeAll(() => server.listen())
+/*
+server.resetHandlers()
+Removes request handlers added in runtime.
+*/
+afterEach(() => server.resetHandlers())
+/*
+  server.close()
+  Stops and cleans up request interception, preventing this
+  interception from affecting all irrelevant tests.
+*/
+afterAll(() => server.close())
+
 // mock window.matchMedia to prevent error due to not being available in jestDom
 beforeAll(() => {
   Object.defineProperty(window, 'matchMedia', {
--- client/src/test/server-handlers.ts
+++ client/src/test/server-handlers.ts
@@ -3,10 +3,11 @@ import articles from './articles.json'
 
 export const handlers = [
   rest.get('http://localhost:3001/api/v1/articles', (req, res, ctx) => {
-    return res(ctx.delay(500), ctx.json({ articles }))
+    return res(ctx.status(200), ctx.delay(500), ctx.json({ articles }))
   }),
   rest.get('http://localhost:3001/api/v1/tags', (req, res, ctx) => {
     return res(
+      ctx.status(200),
       ctx.delay(500),
       ctx.json({
         tags: [
