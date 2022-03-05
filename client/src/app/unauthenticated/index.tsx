0e27a4264d040e306a667ffa69ad71be3077a081
Sfailla <Sfailla1983@gmail.com>
2022-03-05 16:03:34 -0500

    added redirect route for route paths that do not exist

--- client/src/app/unauthenticated/index.tsx
+++ client/src/app/unauthenticated/index.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { ArticlesPage, ArticleDetails, SignUpPage, SignInPage } from '../../pages'
 import { Container } from './style'
 import { Article, Tag } from '../../types/shared'
