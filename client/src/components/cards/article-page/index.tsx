522b725e46ca43770550802eb1683ee3e72017b4
Sfailla <Sfailla1983@gmail.com>
2022-03-09 21:22:37 -0500

    refactored article-view page by removing markup into seperate card component

---
+++ client/src/components/cards/article-page/index.tsx
@@ -0,0 +1,63 @@
+import { Fragment, ReactElement } from 'react'
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { Article } from '../../../types/shared'
+import { convertToReadableDate } from '../../../utils/helperFns'
+import {
+  ArticleTitle,
+  ArticleMeta,
+  ContentWrapper,
+  AuthorName,
+  Avatar,
+  CreatedDate,
+  ArticleBody,
+  MarkdownContainer
+} from './style'
+
+interface Props {
+  article: Article
+}
+
+export default function ArticlePageCard({ article }: Props): ReactElement {
+  return (
+    <Fragment>
+      <ArticleTitle role="heading" aria-level={1}>
+        {article.title}
+      </ArticleTitle>
+
+      <ArticleMeta>
+        <Avatar src={article.image} />
+        <ContentWrapper>
+          <AuthorName>{article.author.username}</AuthorName>
+          <CreatedDate>{convertToReadableDate(article.createdAt)}</CreatedDate>
+        </ContentWrapper>
+      </ArticleMeta>
+
+      <ArticleBody>
+        {/* pass custom styles to ReactMarkdown */}
+        <MarkdownContainer>
+          <ReactMarkdown
+            children={article.body}
+            components={{
+              code({ node, inline, className, children, ...props }) {
+                const match = /language-(\w+)/.exec(className || '')
+                return !inline && match ? (
+                  <SyntaxHighlighter
+                    children={String(children).replace(/\n$/, '')}
+                    style={vscDarkPlus}
+                    language={match[1]}
+                    PreTag="div"
+                    {...props}
+                  />
+                ) : (
+                  <code {...props}>{children}</code>
+                )
+              }
+            }}
+          />
+        </MarkdownContainer>
+      </ArticleBody>
+    </Fragment>
+  )
+}
--- client/src/pages/view-article/index.tsx
+++ client/src/pages/view-article/index.tsx
@@ -1,20 +1,9 @@
 import { ReactElement } from 'react'
 import { useLocation } from 'react-router-dom'
 import { PageContainer, LayoutWrapper } from '../../styles/shared'
-import { convertToReadableDate } from '../../utils/helperFns'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism'
-import {
-  ArticleTitle,
-  ArticleMeta,
-  ContentWrapper,
-  AuthorName,
-  Avatar,
-  CreatedDate,
-  ArticleBody,
-  MarkdownContainer
-} from './style'
-// import ReactMarkdown from 'react-markdown'
+import { ArticlePageCard, CommentSection } from '../../components'
+
+import {} from './style'
 
 export default function ArticleDetails(): ReactElement {
   const {
@@ -24,39 +13,8 @@ export default function ArticleDetails(): ReactElement {
   return (
     <PageContainer>
       <LayoutWrapper>
-        <ArticleTitle role="heading" aria-level={1}>
-          {article.title}
-        </ArticleTitle>
-
-        <ArticleMeta>
-          <Avatar src={article.image} />
-          <ContentWrapper>
-            <AuthorName>{article.author.username}</AuthorName>
-            <CreatedDate>{convertToReadableDate(article.createdAt)}</CreatedDate>
-          </ContentWrapper>
-        </ArticleMeta>
-
-        <ArticleBody>
-          <MarkdownContainer
-            children={article.body}
-            components={{
-              code({ node, inline, className, children, ...props }) {
-                const match = /language-(\w+)/.exec(className || '')
-                return !inline && match ? (
-                  <SyntaxHighlighter
-                    children={String(children).replace(/\n$/, '')}
-                    style={vscDarkPlus}
-                    language={match[1]}
-                    PreTag="div"
-                    {...props}
-                  />
-                ) : (
-                  <code {...props}>{children}</code>
-                )
-              }
-            }}
-          />
-        </ArticleBody>
+        <ArticlePageCard article={article} />
+        <CommentSection />
       </LayoutWrapper>
     </PageContainer>
   )
